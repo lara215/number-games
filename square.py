f2e469cfa306fd28132c4d95dca1e6c67119d26e
Lara Kunze <234050838+lara215@users.noreply.github.com>
2025-09-25 10:39:51 +0200

    solve equation

--- square.py
+++ square.py
@@ -1,9 +1,23 @@
+import numpy as np
+
 def square(number: float) -> float:
     """Return the square of a given number."""
     return number * number
 
+def solve_equation(y,z):
+    return np.sqrt(square(z)-square(y))
+
+def main():
+    solutions=[]
+    for z in range(upper_limit):
+        for y in range(upper_limit):
+            x= solve_equation(y,z)
+            print(x)
+            if x.is_integer():
+                solutions.append([int(x),y,z])
+    print(solutions)
 
 if __name__ == "__main__":
-    num = 3.3
-    result = square(num)
-    print(f"The square of {num} is {result}")
+    upper_limit  = 5
+    main()
+  
